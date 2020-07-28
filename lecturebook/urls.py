222fc2bd3ede1517a410380d8186f51cbefb6bec
AnGyeIn <agistudio97@gmail.com>
2020-07-28 17:14:48 +0900

    2020-07-28(3)

--- lecturebook/urls.py
+++ lecturebook/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	path("students/", views.StudentViewSet.as_view()),
 	path("lecturebooks/", views.LectureBookViewSet.as_view()),
 	path("signup/", views.Signup.as_view()),
-	path("activate/<int:id>", views.ActivateLectureBook.as_view()),
-	path("deactivate/<int:id>", views.DeactivateLectureBook.as_view())
+	path("activate/<int:id>/", views.ActivateLectureBook.as_view()),
+	path("deactivate/<int:id>/", views.DeactivateLectureBook.as_view())
 ]
