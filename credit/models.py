8eafab47493125e06c2762a3122e13c7fd21daee
AnGyeIn <agistudio97@gmail.com>
2020-07-31 16:11:12 +0900

    2020-07-31(2)

--- credit/models.py
+++ credit/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 class CreditData(models.Model):
     user = models.ForeignKey('lecturebook.Student', on_delete=models.CASCADE, to_field='sNum', related_name='creditData')
-    data = models.TextField()
+
+    def __init__(self, data):
+        self.data = data
